9b1152af2d3d670e2faead3cd6f69096bbcea5ee
trudly <trudly@outlook.com>
2017-09-01 15:12:56 +0300

    Bind full cryptonight_hash function

--- src/main/c/cryptonight_jni.c
+++ src/main/c/cryptonight_jni.c
@@ -24,8 +24,33 @@ JNICALL void fastHash(JNIEnv *env, jclass clazz, jbyteArray input, jbyteArray ou
 	(*env)->ReleaseByteArrayElements(env, output, outputBuffer, JNI_COMMIT);
 }
 
+JNICALL void hash(JNIEnv *env, jclass clazz, jbyteArray input, jbyteArray output) {
+	unsigned char* inputBuffer = (*env)->GetByteArrayElements(env, input, NULL);
+	unsigned char* outputBuffer = (*env)->GetByteArrayElements(env, output, NULL);
+
+	jsize inputSize = (*env)->GetArrayLength(env, input);
+	jsize outputSize = (*env)->GetArrayLength(env, output);
+
+//	TODO: check outputSize is 32 (or greater than?)
+
+	if (outputSize < 32) {
+		jclass iole = (*env)->FindClass(env, "net/rapidhashing/cryptonight/InvalidOutputLengthException");
+
+		(*env)->ThrowNew(env, iole, "length of output array is less than 32 bytes");
+		return;
+	}
+
+	cryptonight_hash(inputBuffer, outputBuffer, inputSize);
+
+	(*env)->ReleaseByteArrayElements(env, input, inputBuffer, JNI_ABORT);
+	(*env)->ReleaseByteArrayElements(env, output, outputBuffer, JNI_COMMIT);
+}
+
+
+
 static const JNINativeMethod methods[] = {
 	{ "fastHash", "([B[B)V", (void *) fastHash },
+	{ "hash", "([B[B)V", (void *) hash },
 };
 
 jint JNI_OnLoad(JavaVM *vm, void *reserved) {
@@ -36,7 +61,7 @@ jint JNI_OnLoad(JavaVM *vm, void *reserved) {
 	}
 
 	jclass clazz = (*env)->FindClass(env, "net/rapidhashing/cryptonight/Cryptonight");
-	int r = (*env)->RegisterNatives(env, clazz, methods, 1);
+	int r = (*env)->RegisterNatives(env, clazz, methods, 2);
 
 	return (r == JNI_OK) ? JNI_VERSION_1_8 : -1;
 }
